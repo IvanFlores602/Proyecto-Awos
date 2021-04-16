60a11cebd0ff6a4704202d09ecd9f95dcf36f4f5
Ivan.Lopez <191079.utags.edu.mx>
2021-04-15 19:48:03 -0500

    Proyecto de Awos por Ivan, Gael y Oscar segunda parte

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.get('/', function(req, res) {
     res.send('<h1>Bienvenido a mi servidor REST</h1>');
 });
-
+//// RUTAS DEL PROYECTOq
 app.use(require('./routes/usuario'));
 app.use(require('./routes/categoria'));
 app.use(require('./routes/login'));
